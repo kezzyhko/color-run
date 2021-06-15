ceedbc42d06c9251097426a619f15e0f9fbc58eb
Sergey Semushin <s.semushin@innopolis.university>
2021-06-15 18:19:24 +0300

    Refactor GatherFreeCrowd to create player from prefab

--- Assets/Scripts/GatherFreeCrowd.cs
+++ Assets/Scripts/GatherFreeCrowd.cs
@@ -6,30 +6,26 @@ public class GatherFreeCrowd : MonoBehaviour
 {
 
     public ColorMixing ColorMixing;
+    public GameObject PlayerPrefab;
 
     void OnTriggerEnter(Collider collider)
     {
         // get info
-        GameObject newPlayer = collider.gameObject;
-        Properties props = newPlayer.GetComponent<Properties>();
+        GameObject freeCrowd = collider.gameObject;
+        Properties props = freeCrowd.GetComponent<Properties>();
         if (props == null || props.ObjectType != Properties.Type.FreeCrowd) return;
 
-        // change team
-        props.ObjectType = Properties.Type.Player;
+        // create new player
+        Destroy(freeCrowd);
+        GameObject newPlayer = Instantiate(PlayerPrefab, freeCrowd.transform.position, freeCrowd.transform.rotation);
+        newPlayer.GetComponent<GatherFreeCrowd>().ColorMixing = ColorMixing;
+        newPlayer.GetComponent<GatherFreeCrowd>().PlayerPrefab = PlayerPrefab;
+        newPlayer.GetComponent<ObstacleCollision>().ColorMixing = ColorMixing;
         ColorMixing.Players.AddLast(newPlayer);
 
-        // change color
+        // set color
         Color playerColor = gameObject.GetComponent<Renderer>().material.color;
         newPlayer.GetComponent<Renderer>().material.color = playerColor;
-
-        // add necessary scripts
-        newPlayer.AddComponent<MoveForward>();
-        newPlayer.AddComponent<GatherFreeCrowd>();
-        newPlayer.GetComponent<GatherFreeCrowd>().ColorMixing = ColorMixing;
-        newPlayer.AddComponent<ObstacleCollision>();
-        newPlayer.GetComponent<ObstacleCollision>().ColorMixing = ColorMixing;
-        newPlayer.AddComponent<Rigidbody>();
-        newPlayer.GetComponent<Rigidbody>().isKinematic = true;
     }
 
 }
