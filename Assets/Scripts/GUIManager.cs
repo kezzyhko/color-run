b38dbab95ca90a6e9863b6e7b75fe02a3cd86eda
Sergey Semushin <s.semushin@innopolis.university>
2021-06-17 00:29:04 +0300

    Add Status Text to End Level Screen

--- Assets/Scripts/GUIManager.cs
+++ Assets/Scripts/GUIManager.cs
@@ -6,14 +6,20 @@ public class GUIManager : MonoBehaviour
 {
 
     public GameObject PlayControls;
+
     public GameObject EndLevelScreen;
+    public UnityEngine.UI.Text EndLevelStatusText;
 
     public void EndLevel(bool isWin)
     {
         EndLevelScreen.SetActive(true);
         if (isWin)
         {
-            
+            EndLevelStatusText.text = "Level Complete!";
+        }
+        else
+        {
+            EndLevelStatusText.text = "Try again";
         }
     }
 }
