4f29493172fcea54439887b9ef32a9bc44c0c30a
Sergey Semushin <kezzyhko@gmail.com>
2021-06-25 14:09:19 +0300

    Fix mill recolor

--- Assets/Scripts/ColorUtils/ChangeColor.cs
+++ Assets/Scripts/ColorUtils/ChangeColor.cs
@@ -8,22 +8,13 @@ namespace ColorUtils
     {
 
         public ColorHelper.AcceptableColor ColorName;
-        public GameObject[] ObjectsToRecolor;
 
         void Start()
         {
-            if (ObjectsToRecolor == null || ObjectsToRecolor.Length == 0)
-            {
-                ObjectsToRecolor = new GameObject[] { gameObject };
-            }
-
             Material material = ColorHelper.GetObjectMaterial(gameObject);
             material = Instantiate(material);
             material.color = ColorHelper.EnumToColor(ColorName);
-            foreach (GameObject obj in ObjectsToRecolor)
-            {
-                ColorHelper.SetObjectMaterial(obj, material);
-            }
+            ColorHelper.SetObjectMaterial(gameObject, material);
         }
 
     }
