6ce02bb726f293aab387854cb1cf9bae2d2e6e68
Sergey Semushin <kezzyhko@gmail.com>
2021-06-25 13:24:17 +0300

    Use TryGetComponent

--- Assets/Scripts/Properties.cs
+++ Assets/Scripts/Properties.cs
@@ -17,8 +17,8 @@ public class Properties : MonoBehaviour
 
     public static bool DoesTypeMatch(GameObject obj, Type type)
     {
-        Properties props = obj.GetComponent<Properties>();
-        return props != null && props.ObjectType == type;
+        if (!obj.TryGetComponent(out Properties props)) return false;
+        return props.ObjectType == type;
     }
 
 }
