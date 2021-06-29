365fdccd02ad2bcfdac84a04323840ff605d3f87
Sergey Semushin <kezzyhko@gmail.com>
2021-06-29 19:50:36 +0300

    Add animation to GUI screen changes

--- Assets/Scripts/GUIManager.cs
+++ Assets/Scripts/GUIManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DELTation.UI.Screens;
 
 public class GUIManager : MonoBehaviour
 {
@@ -14,8 +15,8 @@ public class GUIManager : MonoBehaviour
 
     public void ShowScreen(GameObject screen)
     {
-        if (_currentScreen) _currentScreen.SetActive(false);
-        screen.SetActive(true);
+        if (_currentScreen) _currentScreen.GetComponent<GameScreen>().Close();
+        screen.GetComponent<GameScreen>().Open();
         _currentScreen = screen;
     }
 
